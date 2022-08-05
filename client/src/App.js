5089d3c06528951f8663621d42f3b82b06bea3a8
Umer Farooq <umerfarooq03100276796@gmail.com>
2022-08-05 18:26:04 +0500

    Webcam Settled up

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,62 @@
-//Created App
-//Install dependencies ====npm install @tensorflow/tfjs @tensorflow-models/handpose react-webcam
+//Created App ====DONE
+//Install dependencies ====npm install @tensorflow/tfjs @tensorflow-models/handpose react-webcam DONE
+//Import dependencies =====DONE
+//setup webcam ===== DONE
+// Define Refrences =====DONE
+
+import React, {useRef} from 'react';
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
 
 
-
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const webcamRef = useRef(null);
+const canvasRef = useRef(null);
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Webcam ref={webcamRef} 
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zIndex:12,
+          width:1000,
+          height:650,
+          border: '1px solid blue'
+        }}/>
+
+        <canvas
+        ref={canvasRef} 
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zIndex:10,
+          width:1100,
+          height:650,
+          border: '1px solid red'
+        }}
+          />
+
       </header>
     </div>
   );
